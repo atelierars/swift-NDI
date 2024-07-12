11850dca27fdedcb1698c852a422d445104b3102
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-07-13 01:51:27 +0900

    add format

--- Sources/NDILib/Recv.swift
+++ Sources/NDILib/Recv.swift
@@ -58,6 +58,163 @@ extension NDIRecv {
 			return.none
 		}
 		switch frame.FourCC {
+		case NDIlib_FourCC_video_type_UYVY:
+			var result: CVPixelBuffer?
+			return CVPixelBufferCreateWithBytes(
+				kCFAllocatorDefault,
+				.init(frame.xres),
+				.init(frame.yres),
+				kCVPixelFormatType_422YpCbCr8,
+				frame.p_data,
+				.init(frame.line_stride_in_bytes),
+				{(obj, mem) in obj.map(UnsafeRawPointer.init).map(Unmanaged<NDIRecvVideo>.fromOpaque)?.release()},
+				Unmanaged.passRetained(NDIRecvVideo(memory: self, buffer: frame)).toOpaque(),
+				.none,
+				&result) == kCVReturnSuccess ? result : .none
+		case NDIlib_FourCC_video_type_UYVA:
+			var result: CVPixelBuffer?
+			let planeProperties = UnsafeMutablePointer<Int>.allocate(capacity: 6)
+			let planeWidth = planeProperties.advanced(by: 0)
+			let planeHeight = planeProperties.advanced(by: 2)
+			let planeBytesPerRow = planeProperties.advanced(by: 4)
+			let planeBaseAddress = UnsafeMutablePointer<UnsafeMutableRawPointer?>.allocate(capacity: 2)
+			defer {
+				planeProperties.deallocate()
+				planeBaseAddress.deallocate()
+			}
+			planeWidth[0] = .init(frame.xres)
+			planeWidth[1] = .init(frame.xres)
+			planeHeight[0] = .init(frame.yres)
+			planeHeight[1] = .init(frame.yres)
+			planeBytesPerRow[0] = .init(frame.line_stride_in_bytes)
+			planeBytesPerRow[1] = .init(frame.line_stride_in_bytes)
+			planeBaseAddress[0] = .init(frame.p_data)
+			planeBaseAddress[1] = .init(frame.p_data.advanced(by: .init(frame.line_stride_in_bytes * frame.yres)))
+			return CVPixelBufferCreateWithPlanarBytes(
+				kCFAllocatorDefault,
+				.init(frame.xres),
+				.init(frame.yres),
+				kCVPixelFormatType_422YpCbCr_4A_8BiPlanar,
+				frame.p_data,
+				.init(planeBytesPerRow[0] * planeHeight[0] + planeBytesPerRow[1] * planeHeight[1]),
+				2,
+				planeBaseAddress,
+				planeWidth,
+				planeHeight,
+				planeBytesPerRow,
+				{(obj, mem, len, dim, adr) in obj.map(UnsafeRawPointer.init).map(Unmanaged<NDIRecvVideo>.fromOpaque)?.release()},
+				Unmanaged.passRetained(NDIRecvVideo(memory: self, buffer: frame)).toOpaque(),
+				.none,
+				&result) == kCVReturnSuccess ? result : .none
+		case NDIlib_FourCC_video_type_P216:
+			var result: CVPixelBuffer?
+			let planeProperties = UnsafeMutablePointer<Int>.allocate(capacity: 6)
+			let planeWidth = planeProperties.advanced(by: 0)
+			let planeHeight = planeProperties.advanced(by: 2)
+			let planeBytesPerRow = planeProperties.advanced(by: 4)
+			let planeBaseAddress = UnsafeMutablePointer<UnsafeMutableRawPointer?>.allocate(capacity: 2)
+			defer {
+				planeProperties.deallocate()
+				planeBaseAddress.deallocate()
+			}
+			planeWidth[0] = .init(frame.xres)
+			planeWidth[1] = .init(frame.xres / 2)
+			planeHeight[0] = .init(frame.yres)
+			planeHeight[1] = .init(frame.yres / 2)
+			planeBytesPerRow[0] = .init(frame.line_stride_in_bytes)
+			planeBytesPerRow[1] = .init(frame.line_stride_in_bytes)
+			planeBaseAddress[0] = .init(frame.p_data)
+			planeBaseAddress[1] = .init(frame.p_data.advanced(by: planeBytesPerRow[0] * planeHeight[0]))
+			return CVPixelBufferCreateWithPlanarBytes(
+				kCFAllocatorDefault,
+				.init(frame.xres),
+				.init(frame.yres),
+				kCVPixelFormatType_422YpCbCr16BiPlanarVideoRange,
+				frame.p_data,
+				.init(planeBytesPerRow[0] * planeHeight[0] + planeBytesPerRow[1] * planeHeight[1]),
+				2,
+				planeBaseAddress,
+				planeWidth,
+				planeHeight,
+				planeBytesPerRow,
+				{(obj, mem, len, dim, adr) in obj.map(UnsafeRawPointer.init).map(Unmanaged<NDIRecvVideo>.fromOpaque)?.release()},
+				Unmanaged.passRetained(NDIRecvVideo(memory: self, buffer: frame)).toOpaque(),
+				.none,
+				&result) == kCVReturnSuccess ? result : .none
+		case NDIlib_FourCC_video_type_I420:
+			var result: CVPixelBuffer?
+			let planeProperties = UnsafeMutablePointer<Int>.allocate(capacity: 9)
+			let planeWidth = planeProperties.advanced(by: 0)
+			let planeHeight = planeProperties.advanced(by: 3)
+			let planeBytesPerRow = planeProperties.advanced(by: 6)
+			let planeBaseAddress = UnsafeMutablePointer<UnsafeMutableRawPointer?>.allocate(capacity: 3)
+			defer {
+				planeProperties.deallocate()
+				planeBaseAddress.deallocate()
+			}
+			planeWidth[0] = .init(frame.xres)
+			planeWidth[1] = .init(frame.xres / 2)
+			planeWidth[2] = .init(frame.xres / 2)
+			planeHeight[0] = .init(frame.yres)
+			planeHeight[1] = .init(frame.yres / 2)
+			planeHeight[2] = .init(frame.yres / 2)
+			planeBytesPerRow[0] = .init(frame.line_stride_in_bytes)
+			planeBytesPerRow[1] = .init(frame.line_stride_in_bytes / 2)
+			planeBytesPerRow[2] = .init(frame.line_stride_in_bytes / 2)
+			planeBaseAddress[0] = .init(frame.p_data)
+			planeBaseAddress[1] = .init(frame.p_data.advanced(by: planeBytesPerRow[0] * planeHeight[0]))
+			planeBaseAddress[2] = .init(frame.p_data.advanced(by: planeBytesPerRow[0] * planeHeight[0] + planeBytesPerRow[1] * planeHeight[1]))
+			return CVPixelBufferCreateWithPlanarBytes(
+				kCFAllocatorDefault,
+				.init(frame.xres),
+				.init(frame.yres),
+				kCVPixelFormatType_420YpCbCr8PlanarFullRange,
+				frame.p_data,
+				.init(planeBytesPerRow[0] * planeHeight[0] + planeBytesPerRow[1] * planeHeight[1] + planeBytesPerRow[2] * planeHeight[2]),
+				3,
+				planeBaseAddress,
+				planeWidth,
+				planeHeight,
+				planeBytesPerRow,
+				{(obj, mem, len, dim, adr) in obj.map(UnsafeRawPointer.init).map(Unmanaged<NDIRecvVideo>.fromOpaque)?.release()},
+				Unmanaged.passRetained(NDIRecvVideo(memory: self, buffer: frame)).toOpaque(),
+				.none,
+				&result) == kCVReturnSuccess ? result : .none
+		case NDIlib_FourCC_video_type_NV12:
+			var result: CVPixelBuffer?
+			let planeProperties = UnsafeMutablePointer<Int>.allocate(capacity: 6)
+			let planeWidth = planeProperties.advanced(by: 0)
+			let planeHeight = planeProperties.advanced(by: 2)
+			let planeBytesPerRow = planeProperties.advanced(by: 4)
+			let planeBaseAddress = UnsafeMutablePointer<UnsafeMutableRawPointer?>.allocate(capacity: 2)
+			defer {
+				planeProperties.deallocate()
+				planeBaseAddress.deallocate()
+			}
+			planeWidth[0] = .init(frame.xres)
+			planeWidth[1] = .init(frame.xres / 2)
+			planeHeight[0] = .init(frame.yres)
+			planeHeight[1] = .init(frame.yres / 2)
+			planeBytesPerRow[0] = .init(frame.line_stride_in_bytes)
+			planeBytesPerRow[1] = .init(frame.line_stride_in_bytes)
+			planeBaseAddress[0] = .init(frame.p_data)
+			planeBaseAddress[1] = .init(frame.p_data.advanced(by: planeBytesPerRow[0] * planeHeight[0]))
+			return CVPixelBufferCreateWithPlanarBytes(
+				kCFAllocatorDefault,
+				.init(frame.xres),
+				.init(frame.yres),
+				kCVPixelFormatType_420YpCbCr8BiPlanarFullRange,
+				frame.p_data,
+				.init(planeBytesPerRow[0] * planeHeight[0] + planeBytesPerRow[1] * planeHeight[1]),
+				2,
+				planeBaseAddress,
+				planeWidth,
+				planeHeight,
+				planeBytesPerRow,
+				{(obj, mem, len, dim, adr) in obj.map(UnsafeRawPointer.init).map(Unmanaged<NDIRecvVideo>.fromOpaque)?.release()},
+				Unmanaged.passRetained(NDIRecvVideo(memory: self, buffer: frame)).toOpaque(),
+				.none,
+				&result) == kCVReturnSuccess ? result : .none
 		case NDIlib_FourCC_video_type_BGRX, NDIlib_FourCC_video_type_BGRA:
 			var result: CVPixelBuffer?
 			return CVPixelBufferCreateWithBytes(
@@ -71,26 +228,13 @@ extension NDIRecv {
 				Unmanaged.passRetained(NDIRecvVideo(memory: self, buffer: frame)).toOpaque(),
 				.none,
 				&result) == kCVReturnSuccess ? result : .none
-		case NDIlib_FourCC_video_type_RGBX, NDIlib_FourCC_type_RGBA:
+		case NDIlib_FourCC_video_type_RGBX, NDIlib_FourCC_video_type_RGBA:
 			var result: CVPixelBuffer?
 			return CVPixelBufferCreateWithBytes(
 				kCFAllocatorDefault,
 				.init(frame.xres),
 				.init(frame.yres),
 				kCVPixelFormatType_32RGBA,
-				frame.p_data,
-				.init(frame.line_stride_in_bytes),
-				{(obj, mem) in obj.map(UnsafeRawPointer.init).map(Unmanaged<NDIRecvVideo>.fromOpaque)?.release()},
-				Unmanaged.passRetained(NDIRecvVideo(memory: self, buffer: frame)).toOpaque(),
-				.none,
-				&result) == kCVReturnSuccess ? result : .none
-		case NDIlib_FourCC_video_type_UYVY:
-			var result: CVPixelBuffer?
-			return CVPixelBufferCreateWithBytes(
-				kCFAllocatorDefault,
-				.init(frame.xres),
-				.init(frame.yres),
-				kCVPixelFormatType_422YpCbCr8,
 				frame.p_data,
 				.init(frame.line_stride_in_bytes),
 				{(obj, mem) in obj.map(UnsafeRawPointer.init).map(Unmanaged<NDIRecvVideo>.fromOpaque)?.release()},
