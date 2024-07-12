7545717ddeb62f8a89f77e7a696a7dc7d13efb03
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-07-12 20:40:07 +0900

    improve memory management

--- Sources/NDILib/Source.swift
+++ Sources/NDILib/Source.swift
@@ -4,22 +4,33 @@
 //
 //  Created by Kota on 7/10/R6.
 //
-public typealias NDISource = NDIlib_source_t
+public final class NDISource {
+	@usableFromInline
+	let memory: NDISourceCollection
+	@usableFromInline
+	let object: NDIlib_source_t
+	@inlinable
+	init(memory mem: NDISourceCollection, source obj: NDIlib_source_t) {
+		memory = mem
+		object = obj
+	}
+}
 extension NDISource {
 	@inlinable
 	public var name: String {
-		.init(cString: p_ndi_name)
+		.init(cString: object.p_ndi_name)
 	}
 	@inlinable
 	public var url: String {
-		.init(cString: p_url_address)
+		.init(cString: object.p_url_address)
 	}
 	@inlinable
 	public var ip: String {
-		.init(cString: p_ip_address)
+		.init(cString: object.p_ip_address)
 	}
 }
 extension NDISource: CustomStringConvertible {
+	@inlinable
 	public var description: String {
 		name
 	}
