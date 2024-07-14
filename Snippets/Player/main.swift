5588dfa8158dac02906fd921b9e78ab42b65c149
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2024-07-14 10:34:57 +0900

    skip first arguments (binary itself)

--- Snippets/Player/main.swift
+++ Snippets/Player/main.swift
@@ -6,7 +6,7 @@
 //
 import AVFoundation
 import NDILib
-guard let filePath = CommandLine.arguments.first else {
+guard let filePath = CommandLine.arguments.dropFirst().first else {
 	preconditionFailure("Specify Path for Asset via CLI arguments")
 }
 let screen = AVPlayerItemVideoOutput(outputSettings: [
@@ -16,11 +16,12 @@ let playerItem = AVPlayerItem(asset: .init(url: .init(filePath: filePath)))
 playerItem.add(screen)
 
 let player = AVPlayer(playerItem: playerItem)
-player.isMuted = true
-player.play()
-
 let sender = NDISend(video: true)
 let timer = DispatchSource.makeTimerSource(queue: .main)
+timer.setRegistrationHandler {
+	player.isMuted = true
+	player.playImmediately(atRate: 1)
+}
 timer.setEventHandler {
 	if let lastFrame = screen.copyPixelBuffer(forItemTime: player.currentTime(), itemTimeForDisplay: .none) {
 		sender.send(frame: lastFrame)
@@ -28,4 +29,4 @@ timer.setEventHandler {
 }
 timer.schedule(deadline: .now(), repeating: 1001 / 300000.0)
 timer.resume()
-withExtendedLifetime((player, sender, timer), RunLoop.main.run)
+withExtendedLifetime((timer, playerItem), RunLoop.main.run)
